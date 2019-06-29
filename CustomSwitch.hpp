bda23884049ad7b24de43c8046968208575ec3a5
rafauu <gliniakr@gmail.com>
2019-06-29 12:42:22 +0200

    small cleanup and refactoring

--- CustomSwitch.hpp
+++ CustomSwitch.hpp
@@ -1,15 +1,8 @@
 #include <iostream>
-//#include <boost/preprocessor/variadic/size.hpp>
-//#include <boost/preprocessor/variadic/elem.hpp>
-//#include <boost/preprocessor/control/iif.hpp>
 #include <boost/preprocessor/comparison/greater.hpp>
 #include <boost/preprocessor/seq/pop_back.hpp>
-//#include <boost/preprocessor/seq/pop_front.hpp>
 #include <boost/preprocessor/variadic/to_seq.hpp>
-//#include <boost/preprocessor/seq/enum.hpp>
-//#include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
-//#include <boost/preprocessor/seq/seq.hpp>
 #include <boost/preprocessor/seq/reverse.hpp>
 
 #define WithoutLast(...)                       \
@@ -21,13 +14,14 @@
         BOOST_PP_SEQ_REVERSE(                       \
             BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)))
 
-#define SimpleCase(x,y) case x: y; break;
+#define ParticularCase(r, _, elem) case elem:
 
-#define ParticularCase(r, data, elem) case elem:data
-
-#define FallthroughCase(...)                                          \
+#define Case(...)                                                     \
     BOOST_PP_SEQ_FOR_EACH(ParticularCase, , WithoutLast(__VA_ARGS__)) \
-    Last(__VA_ARGS__); break;
+    {                                                                 \
+        Last(__VA_ARGS__);                                            \
+        break;                                                        \
+    }
 
 #define Default(x) default: x; break;
 
@@ -36,7 +30,3 @@
 	{                         \
 	    __VA_ARGS__           \
 	}
-
-#define Case(...) BOOST_PP_IIF(                               \
-    BOOST_PP_GREATER(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), 2), \
-        FallthroughCase, SimpleCase)(__VA_ARGS__)
